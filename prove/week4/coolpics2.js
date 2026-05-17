f4722ecb97eaed8ecbcea584e3e238d84e92a9ea
ContraZombie4 <102491665+ContraZombie4@users.noreply.github.com>
2026-05-16 22:18:14 -0600

    Completed CoolPics2;

--- prove/week4/coolpics2.js
+++ prove/week4/coolpics2.js
@@ -1,11 +1,43 @@
+// Used for Header Menu button
 var menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
-console.log(menuBtn);
-console.log(menu);
 
+// Used for Gallery Modal
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+// Event listener for Menu button
 menuBtn.addEventListener("click", toggleMenu);
 
+// Show or Hide mobile menu
 function toggleMenu() {
     menu.classList.toggle("hide");
     menuBtn.classList.toggle("change")
 }
+
+// Event listener for opening the modal
+gallery.addEventListener('click', openModal);
+
+function openModal(event) {
+    // Code to show modal  - Use event parameter 'e'
+    console.log(event.target.src);
+    let imageSrc = event.target.src;
+
+    // Select img tag inside dialog, give it src
+    modalImage.src = imageSrc.replace("-sm.", "-full.");
+    // console.log(typeof(imageSrc))   //NOTE: typeof() can be used to find the type of a variable
+    modal.showModal();
+}
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+// Close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
