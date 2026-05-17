f21045b3e54398a684c86ed5cd7627ad98649ce0
ContraZombie4 <102491665+ContraZombie4@users.noreply.github.com>
2026-05-16 21:04:09 -0600

    Added Menu button functionality from Ponder Activity;

---
+++ prove/week4/coolpics2.js
@@ -0,0 +1,11 @@
+var menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+console.log(menuBtn);
+console.log(menu);
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    menu.classList.toggle("hide");
+    menuBtn.classList.toggle("change")
+}
